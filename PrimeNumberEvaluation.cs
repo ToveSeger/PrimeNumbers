06c4fbd127875a4cfbc36f4e851b3a475fec9ce4
toveseger <tove.seger@outlook.com>
2021-10-05 21:19:13 +0200

    Add project files.

---
+++ PrimeNumberEvaluation.cs
@@ -0,0 +1,32 @@
+﻿namespace PrimeNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class PrimeNumberEvaluation
+    {
+        private List<int> PrimeNumberList = new List<int>();
+
+        public string PrimeNumberCheck(int number)
+        {
+            if (number <= 1) return "This is not a prime number";
+            if (number % 2 == 0 && number != 2) return "This is not a prime number";
+            else PrimeNumberList.Add(number);
+            return "This is a prime number";
+        }
+
+        public void PrintList()
+        {
+            for (int i = 0; i < PrimeNumberList.Count; i++)
+            {
+                Console.WriteLine(PrimeNumberList[i]);
+            }
+        }
+
+        public void AddNextPrimeNumber()
+        {
+            int currentHighestNumber = PrimeNumberList[PrimeNumberList.Count - 1];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PrimeNumbers
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeNumberEvaluation pne = new PrimeNumberEvaluation();
+            string evaluator= pne.PrimeNumberCheck(17);
+            Console.WriteLine(evaluator);
+            pne.PrintList();
+        }
+    }
+}
